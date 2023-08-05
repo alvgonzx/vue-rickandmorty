28bb82ac58f860308d53a5fa2414930aa4767f15
Álvaro González <alvgonz.ojeda@gmail.com>
2023-08-05 15:47:35 +0200

    [dev] finished functionality, images and next and previous buttons

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,10 @@ import { ref } from "vue";
 import Character from "./components/Character.vue";
 
 const characters = ref([]);
-const page = ref(1);
+const page = ref(0);
+
+const DISABLED_PREVIOUS = "previous";
+const DISABLED_NEXT = "next";
 
 const fetchCharacters = async (page) => {
     const response = await fetch(
@@ -14,9 +17,26 @@ const fetchCharacters = async (page) => {
     characters.value = data.results;
 };
 
-fetchCharacters(page.value);
-</script>
+const previous = () => {
+    page.value--;
+    fetchCharacters(page.value);
+};
 
+const next = () => {
+    page.value++;
+    fetchCharacters(page.value);
+};
+
+const checkDisabled = (type) => {
+    if (type === DISABLED_PREVIOUS && page.value === 1) {
+        return true;
+    } else if (type === DISABLED_NEXT && page.value === 42) {
+        return true;
+    } else return false;
+};
+
+next();
+</script>
 
 <template>
     <header>
@@ -31,8 +51,31 @@ fetchCharacters(page.value);
     </header>
 
     <main>
-        <div class="flex lg:flex-wrap flex-col lg:flex-row lg:w-full" >
-            <Character v-for="character in characters" :key="character.id" :name="character.name" :status="character.status" :species="character.species" img="" />
+        <div class="flex lg:flex-wrap flex-col lg:flex-row lg:w-full">
+            <Character
+                v-for="character in characters"
+                :key="character.id"
+                :name="character.name"
+                :status="character.status"
+                :species="character.species"
+                :img="character.image"
+            />
+        </div>
+        <div class="flex justify-center pb-4">
+            <button
+                @click="previous"
+                class="bg-yellow-500 text-white text-center mx-auto py-2 px-4 rounded-md disabled:bg-gray-300 disabled:text-gray-100"
+                :disabled="checkDisabled(DISABLED_PREVIOUS)"
+            >
+                Previous page
+            </button>
+            <button
+                @click="next"
+                class="bg-red-500 text-white text-center mx-auto py-2 px-4 rounded-md disabled:bg-gray-300 disabled:text-gray-100"
+                :disabled="checkDisabled(DISABLED_NEXT)"
+            >
+                Next page
+            </button>
         </div>
     </main>
 </template>
--- src/components/Character.vue
+++ src/components/Character.vue
@@ -5,7 +5,9 @@ const props = defineProps(["img", "name", "status", "species"]);
 <template>
     <article class="w-full lg:w-1/4 p-4">
         <div class="bg-white rounded-lg shadow-lg">
-            <div class="bg-gray-400 h-64 rounded-t-lg"></div>
+            <div class="h-64 rounded-t-lg">
+                <img :src="img">
+            </div>
             <div class="p-4">
                 <h2 class="text-2xl font-bold">{{ name }}</h2>
                 <p class="text-gray-600">{{ species }} · {{ status }}</p>
