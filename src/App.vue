5d695b9ad38f137036eeaa267663bb98b77df694
Álvaro González <alvgonz.ojeda@gmail.com>
2023-08-05 12:46:21 +0200

    [dev] basic cards and first fetch to api

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,36 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import Character from "./components/Character.vue";
+
+const fetchCharacters = async (page) => {
+    const response = await fetch(
+        `https://rickandmortyapi.com/api/character?page=${page}`
+    );
+    const data = await response.json();
+    console.log(data);
+};
+
+fetchCharacters(4);
+</script>
 
 <template>
     <header>
-        <h1 class="text-5xl py-4 text-center bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent font-bold">Rick and morty</h1>
+        <h1
+            class="text-5xl pt-4 pb-1 text-center bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent font-bold"
+        >
+            Rick and morty
+        </h1>
+        <p class="text-xl font-medium text-center">
+            Rick and morty API project for learning VueJS
+        </p>
     </header>
 
-    <main></main>
+    <main>
+        <div class="flex flex-col lg:flex-row lg:w-full">
+            <Character />
+            <Character />
+            <Character />
+            <Character />
+        </div>
+    </main>
 </template>
---
+++ src/components/Character.vue
@@ -0,0 +1,14 @@
+// character component
+<script setup></script>
+
+<template>
+    <article class="w-full p-4">
+        <div class="bg-white rounded-lg shadow-lg">
+            <div class="bg-gray-400 h-64 rounded-t-lg"></div>
+            <div class="p-4">
+                <h2 class="text-2xl font-bold">Rick Sanchez</h2>
+                <p class="text-gray-600">Human · Alive</p>
+            </div>
+        </div>
+    </article>
+</template>
